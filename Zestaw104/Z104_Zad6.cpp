0927a6fc6c6c2476db17d7e360731c9d33b54b76
Shadoweee77 <itzshadowitzshadow@gmail.com>
2023-01-31 19:45:37 +0100

    2+2 = 5

--- Zestaw104/Z104_Zad6.cpp
+++ Zestaw104/Z104_Zad6.cpp
@@ -5,10 +5,7 @@ using namespace std;
 int funkcja(int n) {
 	if (n == 0) return 1;
 	if (n == 1) return 2;
-	if (n > 0) {
-		if (2*n % 2 == 0) return funkcja(n) + 2;
-		if (2*n % 2 == 1) return 2 * funkcja(2*n - 1);
-	}
+	return n % 2 == 0 ? funkcja(n/2) + 2 : 2 * funkcja(n-2);
 }
 
 int main()
